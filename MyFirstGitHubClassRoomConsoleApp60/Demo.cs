9b55634918f1fc16b617131f211d12cdd1b07903
Alex G. Jensen <jindoohex@hotmail.com>
2022-09-05 13:07:37 +0200

    Added Demo class & DemoWorker

---
+++ MyFirstGitHubClassRoomConsoleApp60/Demo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstGitHubClassRoomConsoleApp60
+{
+    public class Demo
+    {
+        #region Instance Fields
+        private string? _name;
+        private int _age;
+        #endregion
+
+        #region Properties
+        public string Name { get; set; }
+        public int Age { get; set; }
+        #endregion
+
+        #region Constructors
+        public Demo()   // Default constructor
+        {
+        }
+
+        public Demo(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+        #endregion
+
+        #region Methods
+        public override string ToString()
+        {
+            return $"{nameof(Name)}: {Name}, {nameof(Age)}: {Age}";
+        }
+        #endregion
+    }
+}
---
+++ MyFirstGitHubClassRoomConsoleApp60/DemoWorker.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstGitHubClassRoomConsoleApp60
+{
+    public class DemoWorker
+    {
+        internal void Start()
+        {
+        }
+    }
+}
